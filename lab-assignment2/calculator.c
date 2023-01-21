0d8e13f8674890273e4e24288b71bb1ab8cd9f7f
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-01-20 21:46:12 -0800

    upload lab assignment2

--- lab-assignment2/calculator.c
+++ lab-assignment2/calculator.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+//Minjia Tao
+//tao.mi@northeastern.edu
 #include<stdio.h>
 int main()
 {
@@ -8,11 +8,45 @@ int main()
     printf("Enter your choice\n");
     printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
     scanf("%d",&choice);
-    
-    printf("Enter a and b values");
-    scanf("%d %d", &a, &b);
-    
+    if (!(choice >= 1 && choice <= 4)) { //注意这边if语句的写法
+        printf("Invalid option\n");
     // Add your code here using switch
-           
+    } else {
+        printf("Enter a and b values");
+        scanf("%d %d", &a, &b);
+        switch (choice) {
+            case 1://变成integer在switch和case里面
+                printf("Addition\n");
+                c = a + b;
+                printf("Sum=%d\n", c);
+                break;
+
+            case 2:
+                printf("Subtraction\n");
+                c = a - b;
+                printf("Difference=%d\n", c);
+                break;
+                
+            case 3:
+                printf("Multiplication\n");
+                c = a*b;
+                printf("Product=%d\n", c);
+                break;
+        
+            case 4:
+                
+                if (b == 0) { //要用括号而不是冒号，改了之后要重新编译
+                    printf("divide by zero error\n");
+                    break;
+                }
+                else {
+                    c = a/b;
+                    printf("Division\n");
+                    printf("Quotient=%d\n",c);
+                    break;
+                }
+        }
+    }
+        
     return 0;
 }
--- lab-assignment2/employee.c
+++ lab-assignment2/employee.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*structure to hold the details of an employee*/
+
 typedef struct employee
 {
     int empId;
@@ -11,14 +12,32 @@ typedef struct employee
 
 }employee_t;
 /* Read the employee details using scanf*/
-void readData(int n, employee_t* s)
-{
+void readData(int n, employee_t* s) //what does it mean by employee_t* s s== array，每一个element定义
+{ //s 是形参
     // Add your code here
+    for(int i = 0; i < n; i++) {
+        printf("\nEnter the employeeID, Name, designation and Department of Employee %d:", i + 1);
+        scanf("%d%s%s%s", &s[i].empId, s[i].Name, s[i].Designation, s[i].Dept); //为什么只有一个指针, consider the next line here
+        //不用空格，也不要/n读取
+        //s是一个数组，是一个头指针
+        // s数组中第n个数组访问
+        // s数组的位置。 解引用 dereference 用&这个标志
+        // s是个首元素的地址
+    }
 }
 /* Function to print the employee details*/
 void display(int n, employee_t * s)
 {
     // Add your code here
+    for(int i = 0; i < n; i++){
+        printf("\nThe details of Employee %d:\n", i + 1);
+        printf("empId: %d\n", s[i].empId);
+        printf("name: %s\n", s[i].Name);
+        printf("Designation: %s\n", s[i].Designation);
+        printf("Dept: %s\n", s[i].Dept);//s相当于一个数组，s[i]取不同数组的位置，对每个元素进行取值
+        printf("\n"); //不能保证一直换行
+
+    }
     
 }
 
@@ -28,7 +47,17 @@ int main()
 {
     // Main Function to print the employee details
     // Add your code here
+    // malloc
     
+    printf("Enter the number of employees:");
+    int n; // 先初始化变量，初始
+    scanf("%d", &n); //读入变量scanf，往地址里面存，不是一个变量
+    employee_t* q = (employee_t *) malloc(sizeof(employee_t) * n);//when to use malloc function, pointer, memory，创建一个n大小的employee_t
+    //没有空格，指针类型的变量， n个大小为employee_t的变量
+    readData(n, q);
+    display(n, q);
+    free(q);//需要释放变量，避免内存泄漏
+    return 0;//一定要返回变量return 0，因为main函数得return 0
 
 }
 
